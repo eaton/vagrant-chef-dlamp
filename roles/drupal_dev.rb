4e6bba9f21c9a5c656a426ecedf99c58d0a29474
art kontreras <arturo.kontreras@gmail.com>
2011-08-26 12:47:20 -0500

    Issue with drush fixed: Drupal Dev cookbook needed updating because drush location was moved, now we will use drush head to point to correct location.

--- roles/drupal_dev.rb
+++ roles/drupal_dev.rb
@@ -2,7 +2,7 @@ name "drupal_dev"
 description "Useful tools for Drupal developers."
 run_list(
   "recipe[drupal::dev]",
-  "recipe[drush]",
+  "recipe[drush::head]",
   "recipe[drush_make]",
   "recipe[phpmyadmin]", # TODO Cookbook needs testing!
   "recipe[webgrind]", # TODO Does this actually work?
